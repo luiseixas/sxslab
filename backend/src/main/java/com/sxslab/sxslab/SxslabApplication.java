38fa6e65aca7615cd6d9e1bf6b825d921bb7e547
Luís Seixas <luiseixas2@gmail.com>
2022-09-07 22:24:23 -0300

    Initializing project.

---
+++ backend/src/main/java/com/sxslab/sxslab/SxslabApplication.java
@@ -0,0 +1,13 @@
+package com.sxslab.sxslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SxslabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SxslabApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sxslab/sxslab/SxslabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxslab.sxslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SxslabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
