51472fce9ead5cf4d9b0bc3b12b95c36dc9f542e
Luís Seixas <luiseixas2@gmail.com>
2022-09-09 17:58:16 -0300

    feat: implement TWILIO SMS

--- backend/src/main/java/com/sxslab/sxslab/controllers/SaleController.java
+++ backend/src/main/java/com/sxslab/sxslab/controllers/SaleController.java
@@ -1,15 +1,18 @@
 package com.sxslab.sxslab.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sxslab.sxslab.entities.Sale;
 import com.sxslab.sxslab.services.SaleService;
+import com.sxslab.sxslab.services.SmsService;
 
 @RestController
 @RequestMapping(value="/sales")
@@ -18,6 +21,9 @@ public class SaleController {
 	@Autowired
 	private SaleService service;
 	
+	@Autowired
+	private SmsService smsService;
+	
 	@GetMapping
 	public Page<Sale> findSales(
 			@RequestParam(value="minDate", defaultValue = "") String minDate,
@@ -25,4 +31,10 @@ public class SaleController {
 			Pageable pageable) {
 		return service.findSales(minDate, maxDate, pageable);
 	}
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
+	}
+	
 }
